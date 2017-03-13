038e4c62b1c44bce28e300be45042d0eabfffaba
ghostorm <valnolife@gmail.com>
2017-03-13 15:35:18 +0100

    Index
    Commit index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<title>Insert title here</title>
+	<link rel="stylesheet" type="text/css" href="fichier.css">
+	</head>
+
+	<body>
+		<header>
+		
+		</header>
+
+		<main>
+			
+		</main>
+
+		<footer>
+		
+		</footer>
+	</body>
+</html>
